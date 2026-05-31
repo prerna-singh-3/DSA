700b2c0e68f84761b5344f24b66e320dbe93e186
Prerna Singh <prernasinghhh22@gmail.com>
2026-05-31 22:54:37 +0530

    Switch Statement

---
+++ p4.cpp
@@ -0,0 +1,41 @@
+/*
+Take the day no and print the corresponding day
+for 1 print Monday,
+for 2 print Tuesday and so on for 7 print Sunday.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int day;
+    cin >> day;
+    switch (day)
+    {
+    case 1:
+        cout << "Monday";
+        break;
+    case 2:
+        cout << "Tuesday";
+        break;
+    case 3:
+        cout << "Wednesday";
+        break;
+    case 4:
+        cout << "Thursday";
+        break;
+    case 5:
+        cout << "Friday";
+        break;
+    case 6:
+        cout << "Saturday";
+        break;
+    case 7:
+        cout << "Sunday";
+        break;
+    }
+    return 0;
+}
