dbf935657c6ebf25ba5f67bf387ece0a5f922649
Prerna Singh <prernasinghhh22@gmail.com>
2026-06-01 00:42:32 +0530

    Arrays

---
+++ p6.cpp
@@ -0,0 +1,34 @@
+// ARRAYS (1D) //
+
+/* #include <iostream>
+using namespace std;
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int arr[5];
+    cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4];
+    arr[2] += 12;
+    cout << arr[2];
+    return 0;
+} */
+
+
+
+// AARAYS (2D) //
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int arr[3] [5];
+
+    arr[1] [3] = 27;
+    cout << arr[1] [3];
+    return 0;
+}
+
